baf04d1c40d3f3615ef5b73fa4d650d76b6f247a
DaniloVrancic <danilov98@hotmail.com>
2024-01-10 20:04:27 +0100

    IP 1.1: Initialized backend project with Spring Initializr

---
+++ gym.backend/onlinefitnessmanager/src/test/java/org/unibl/etf/onlinefitnessmanager/OnlinefitnessmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unibl.etf.onlinefitnessmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinefitnessmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
