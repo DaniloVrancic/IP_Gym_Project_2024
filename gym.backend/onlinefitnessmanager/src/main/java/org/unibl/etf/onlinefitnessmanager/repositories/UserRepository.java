e0be39eabb233586366508d000fddf320444f581
DaniloVrancic <danilov98@hotmail.com>
2024-03-21 19:35:31 +0100

    IP 1.9.15: Added calling outside API daily. Won't call unnecessarily if file already exists. Sending those files in text json format to REST endpoint.

--- gym.backend/onlinefitnessmanager/src/main/java/org/unibl/etf/onlinefitnessmanager/repositories/UserRepository.java
+++ gym.backend/onlinefitnessmanager/src/main/java/org/unibl/etf/onlinefitnessmanager/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package org.unibl.etf.onlinefitnessmanager.repositories;
 
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.unibl.etf.onlinefitnessmanager.model.entities.RecommendedExcerciseEntity;
 import org.unibl.etf.onlinefitnessmanager.model.entities.UserEntity;
-
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
