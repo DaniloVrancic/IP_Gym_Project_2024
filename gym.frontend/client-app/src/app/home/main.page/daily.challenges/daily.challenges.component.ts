9faa89f937c691d983863deaa04e3d8da027725c
DaniloVrancic <danilov98@hotmail.com>
2024-03-24 23:57:00 +0100

    Update dev state

--- gym.frontend/client-app/src/app/home/main.page/daily.challenges/daily.challenges.component.ts
+++ gym.frontend/client-app/src/app/home/main.page/daily.challenges/daily.challenges.component.ts
@@ -23,7 +23,7 @@ export class DailyChallengesComponent {
   {
     this.dailyChallenges = JSON.parse(sessionStorage.getItem(environment.dailyChallengesKeyString) as string);
 
-    if(this.dailyChallenges)
+    if(this.dailyChallenges) //If already in storage
     {
       this.displayedDailyChallenges = this.dailyChallenges.slice(0,1);
     }
@@ -33,8 +33,8 @@ export class DailyChallengesComponent {
       dailyChallengeService.getTodaysDailyChallenges().subscribe(response => {
                                                                               response.forEach(element => {this.dailyChallenges?.push(element)});
                                                                               sessionStorage.setItem(environment.dailyChallengesKeyString, JSON.stringify(this.dailyChallenges));
-                                                                             })
-      this.displayedDailyChallenges = this.dailyChallenges.slice(0,1);                                                       
+                                                                              this.displayedDailyChallenges = this.dailyChallenges?.slice(0,1) as DailyChallenge[] | null;
+                                                                             })                                                       
     }
   }
 
