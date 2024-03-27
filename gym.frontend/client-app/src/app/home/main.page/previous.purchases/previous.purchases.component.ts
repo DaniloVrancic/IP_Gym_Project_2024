7b9f14ee9fb33f0b4ec722839e63d52a79ce4622
DaniloVrancic <danilov98@hotmail.com>
2024-03-27 17:45:31 +0100

    IP 1.10: Fixed bug where ID was referenced wrongly.

--- gym.frontend/client-app/src/app/home/main.page/previous.purchases/previous.purchases.component.ts
+++ gym.frontend/client-app/src/app/home/main.page/previous.purchases/previous.purchases.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../user.service';
+import { PurchaseService } from '../purchase.service';
+import { FitnessProgramService } from '../fitness-program.service';
+import { FitnessProgram } from '../fitness-program';
 
 @Component({
   selector: 'app-previous-purchases',
   standalone: true,
   imports: [],
   templateUrl: './previous.purchases.component.html',
-  styleUrl: './previous.purchases.component.css'
+  styleUrl: './previous.purchases.component.css',
+  providers: [UserService, PurchaseService, FitnessProgramService]
 })
-export class PreviousPurchasesComponent {
+export class PreviousPurchasesComponent implements OnInit{
 
+
+  private purchases: any[];
+  public purchasedPrograms: FitnessProgram[];
+
+  constructor(private userService: UserService, private purchaseService: PurchaseService, private fitnessProgramService: FitnessProgramService)
+  {
+    this.purchases = [];
+    this.purchasedPrograms = [];
+  }
+
+  ngOnInit(): void {
+      this.purchaseService.findAll().subscribe(response => console.log(response));
+
+      this.purchaseService.findByUserId(this.userService.getCurrentUser()?.id as number).subscribe(response => {
+        this.purchases = response;
+        for(let purchase of this.purchases) { 
+          this.fitnessProgramService.findFitnessProgramById(purchase.fitnessProgramId as number)
+                                    .subscribe(response => this.purchasedPrograms.push(response))
+                                  }});
+  }
 }
