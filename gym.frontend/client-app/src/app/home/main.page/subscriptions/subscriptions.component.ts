7f200d051e7a357efcca6266dd145a96ded6de35
DaniloVrancic <danilov98@hotmail.com>
2024-04-04 15:49:11 +0200

    IP 1.12.4: Started subscription page. Getting correct subscriptions and displaying them.

--- gym.frontend/client-app/src/app/home/main.page/subscriptions/subscriptions.component.ts
+++ gym.frontend/client-app/src/app/home/main.page/subscriptions/subscriptions.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SubscribeService } from './subscribe.service';
+import { UserService } from '../../../user.service';
+import { FitnessProgramTypeService } from '../fitness-program-type.service';
+import { FitnessProgramType } from '../fitness-program-type';
 
 @Component({
   selector: 'app-subscriptions',
   standalone: true,
   imports: [],
   templateUrl: './subscriptions.component.html',
-  styleUrl: './subscriptions.component.css'
+  styleUrl: './subscriptions.component.css',
+  providers: [SubscribeService, UserService, FitnessProgramTypeService]
 })
-export class SubscriptionsComponent {
+export class SubscriptionsComponent implements OnInit {
 
+
+  public subscriptions: any[];
+  fitnessTypes: FitnessProgramType[];
+  public fitnessTypeMap: Map<number,string>; //ID to name mapping, (alias mapping)
+
+
+  constructor(subscribeService: SubscribeService, 
+              userService: UserService, fitnessTypeService: FitnessProgramTypeService)
+  {
+    this.subscriptions = [];
+    this.fitnessTypes = [];
+    this.fitnessTypeMap = new Map<number,string>();
+    subscribeService.getSubscriptionsByUserId(userService.getCurrentUser()?.id as number)
+                                      .subscribe(allSubs => {this.subscriptions = allSubs; console.log(this.subscriptions);});
+    fitnessTypeService.getAllFitnessProgramTypes().subscribe(allTypes => {this.fitnessTypes = allTypes; this.fitnessTypes.forEach(x => this.fitnessTypeMap.set(x.id as number, x.name)); console.log(this.fitnessTypeMap)});
+
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+  onClick() {
+    console.log(this.subscriptions);
+    }
 }
