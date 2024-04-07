cf3c4d959c23daa909069c4611de0987d0d691ca
DaniloVrancic <danilov98@hotmail.com>
2024-04-07 17:55:38 +0200

    IP 1.15: Finished the feature of checking if the excercise is online. If it is online and bought, open a new link to the youtube video with an excercise.

--- gym.frontend/client-app/src/app/home/purchase.page/purchase.page.component.ts
+++ gym.frontend/client-app/src/app/home/purchase.page/purchase.page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MaterialModule } from '../../material/material.module';
 import { Router } from '@angular/router';
 import { MatRadioButton, MatRadioGroup, MatRadioModule } from '@angular/material/radio';
@@ -19,15 +19,12 @@ import { UserService } from '../../user.service';
 })
 export class PurchasePageComponent implements OnInit{
 
-
-
   paymentMethod: string; //Will be set by the radio group
-  payMethods: string[] = ['Credit Card', 'PayPal', 'In person'];
+  payMethods: string[] = ['Credit-Card', 'PayPal', 'In-person'];
 
 
   creditCardNumberControl = new FormControl('', [Validators.required]);
   paypalInputControl = new FormControl('', [Validators.required, Validators.email]);
- 
 
   matcher = new MyErrorStateMatcher(); //Error matcher, got it from another class
 
@@ -37,7 +34,7 @@ export class PurchasePageComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    
+   // let inPersonRadioButton : any = document.getElementById('radio-button-In-person-input');
   }
 
   isButtonEnabled(): boolean {
@@ -64,8 +61,31 @@ export class PurchasePageComponent implements OnInit{
     }
 
   purchaseClick() {
+
     this.purchaseService.addPurchase((this.userService.getCurrentUser()?.id as number), (this.fitnessProgramService.getCurrentFitnessProgram()?.id as number)).subscribe(() => {
-                                                                                                                                                                                alert("Successful purchase!"); 
-                                                                                                                                                                                this.router.navigate(['/main-page'])});
+                      alert("Successful purchase!");
+                       if(this.fitnessProgramService.getCurrentFitnessProgram()?.locationOfEvent?.toLowerCase() === "online") //case if the program is online
+                       {
+                       const randomExcerciseVideoLink = 
+                       [
+                         'https://www.youtube.com/watch?v=XxuRSjER3Qk',
+                         'https://www.youtube.com/watch?v=c1mBu4tK90k',
+                         'https://www.youtube.com/watch?v=J212vz33gU4',
+                         'https://www.youtube.com/watch?v=kZDvg92tTMc'
+                       ];
+                       //Generate random index in the array range
+                       const randomIndex = Math.floor(Math.random() * randomExcerciseVideoLink.length);
+
+                       //Choose the link with the randomly generated index:
+                       const randomLink: string = randomExcerciseVideoLink[randomIndex];
+
+                       this.router.navigate(['/main-page']);
+                       window.open(randomLink, '_blank');
+                       }
+                     else //case if the program is anything else but 'online'
+                     {
+                     this.router.navigate(['/main-page']);
+                     }
+                   });
     }
 }
